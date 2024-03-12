89535eb722ebe6da63499a18e33a086dcfb4fd77
roshan2345 <jha062305@gmail.com>
2024-03-12 18:32:37 +0530

    added utility classes

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 8000,()=>{
+    console.log(`server is Running at Port : ${process.env.PORT}`);
+  })
+})
+.catch((err) => console.log("Mongo DB connection failed!!!!!",err));
 
 
 
