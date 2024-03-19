f254c4825e63ed466198a3b8425d65bcce892689
roshan2345 <jha062305@gmail.com>
2024-03-20 01:24:11 +0530

    password changer

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -202,5 +202,21 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     throw new ApiError(401, "Invalid  Token");
   }
 });
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+
+  const user = await User.findById(req?.user._id);
+
+  const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+  if (!isPasswordCorrect) {
+    throw new ApiError(401, "Old Password is Incorrect");
+  }
+  user.password = newPassword;
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password change Successfully"));
+});
 
 export { registerUser, loginUser, logoutUser, refreshAccessToken };
