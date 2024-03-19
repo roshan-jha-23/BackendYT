f98e2ec8de51b3f485fc628ac500eb01e266d653
roshan2345 <jha062305@gmail.com>
2024-03-19 23:52:00 +0530

    addedrefresh token genratoin

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -168,13 +168,38 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   if (!incomingRefreshToken) {
     throw new ApiError(403, "Unauthorized Request");
   }
-  const decodedToken = jwt.verify(
-    incomingRefreshToken,
-    process.env.REFRESH_TOKEN_SECRET
-  );
-  let user = await User.findById(decodedToken?._id)
-  if(!user){
-    throw new ApiError(404,"Invalid Token")
+  try {
+    const decodedToken = jwt.verify(
+      incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+    let user = await User.findById(decodedToken?._id);
+    if (!user) {
+      throw new ApiError(404, "Invalid Token");
+    }
+    if (incomingRefreshToken !== user.refreshToken) {
+      throw new ApiError(401, "Refresh token iss expired or used");
+    }
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    const { accessToken, newRefreshToken } =
+      await generateAccessAndRefreshToken(user._id);
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", newRefreshToken, options)
+      .json(
+        new ApiResponse(
+          200,
+          { accessToken, newRefreshToken },
+          "Access Token Refreshed SuccessFully"
+        )
+      );
+  } catch (error) {
+    throw new ApiError(401, "Invalid  Token");
   }
 });
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,27 +1,32 @@
 import { Router } from "express";
-import {loginUser, logoutUser, registerUser} from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-const router=Router();
+const router = Router();
 
-router.route('/register').post(
-    upload.fields([
-     {
-        name: 'avatar', 
-        maxCount: 1,
-     },
-     {
-        name:"coverImage",
-        maxCount: 1
-     }
-    ]),
-    registerUser)
-router.route('/login').post(loginUser)    
-
-
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
+router.route("/login").post(loginUser);
 
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 
 
-export default router
+export default router;
